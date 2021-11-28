84897f87ae62aa31bd11a2ce2343b4f899ea5121
Jorge Burgos <burgosjorge2907@gmail.com>
2021-11-28 15:58:55 -0500

    heroes Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +17,8 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HeroesModule
+    HeroesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
