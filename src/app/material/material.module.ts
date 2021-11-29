780e449382bcb82ce663ea69dc1759604344f2a5
Jorge Burgos <burgosjorge2907@gmail.com>
2021-11-28 22:56:36 -0500

    Tarea Heroe Component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
-
-
-
+import {MatCardModule} from '@angular/material/card';
 
 
 
@@ -21,7 +18,8 @@ import {MatListModule} from '@angular/material/list';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
