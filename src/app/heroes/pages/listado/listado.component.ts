07e5138edb91a2b2a9d4941c255381871164a1b3
Jorge Burgos <burgosjorge2907@gmail.com>
2021-11-28 22:17:55 -0500

    Interfaz heroe e impresion nombres

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interfaces';
 
 @Component({
   selector: 'app-listado',
@@ -9,12 +10,15 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
+
   constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
 
     this.heroesService.getHeroes()
-    .subscribe( resp => console.log( resp ));
+    .subscribe( resp => this.heroes = resp);
 
   }
 
