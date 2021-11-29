8b8fc388987c90a94e495cb1b9dc1f82bb534579
Jorge Burgos <burgosjorge2907@gmail.com>
2021-11-29 11:37:05 -0500

    pantalla Heroes

---
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Heroe } from '../interfaces/heroes.interfaces';
+
+@Pipe({
+  name: 'imagen'
+})
+export class ImagenPipe implements PipeTransform {
+
+  transform( heroe: Heroe ): string {
+
+    console.log(heroe);
+    return `assets/heroes/${heroe.id}.jpg`;
+
+  }
+
+}
