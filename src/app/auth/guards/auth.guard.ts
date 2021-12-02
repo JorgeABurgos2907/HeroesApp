7c289f99debd04e60b4047d7797ea2ad1c22d56b
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-01 20:30:00 -0500

    Fin seccion 16

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,27 +1,52 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Injectable, Pipe } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  CanLoad {
+export class AuthGuard implements  CanLoad, CanActivate {
 
-  constructor( private authService:AuthService ){}
+  constructor( private authService:AuthService, private router:Router ){}
 
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    //   if(this.authService.auth.id){
+    //     return true;
+    //   }
+    //   console.log('Bloqueado Can Activate')
+    // return false;
+
+    return this.authService.verificaAutenticacion()
+        .pipe(
+          tap( estaAutenticado => {
+            if(!estaAutenticado){
+              this.router.navigate(['./auth/login'])
+            }
+          })
+        )
+  }
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean  {
 
-      if(this.authService.auth.id){
-        return true;
-      }
-    return false;
+      return this.authService.verificaAutenticacion().pipe(
+        tap( estaAutenticado => {
+          if(!estaAutenticado){
+            this.router.navigate(['./auth/login'])
+          }
+        })
+      )
+
+
+    //   if(this.authService.auth.id){
+    //     return true;
+    //   }
+    //   console.log('Bloqueado Can Load')
+    // return false;
   }
 }
